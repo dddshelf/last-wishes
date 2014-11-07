82b17e6644c4c420654ba23fc0fc069bd810a7fb
Carlos Buenosvinos <hi@carlos.io>
2014-11-07 11:47:59 +0100

    Push Notification Command

--- src/Lw/Infrastructure/Ui/Console/Command/PushNotificationsCommand.php
+++ src/Lw/Infrastructure/Ui/Console/Command/PushNotificationsCommand.php
@@ -7,12 +7,12 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class StartWorkerCommand extends Command
+class PushNotificationsCommand extends Command
 {
     protected function configure()
     {
         $this
-            ->setName('notifications:push')
+            ->setName('lw:notifications:push')
             ->setDescription('Notify all domain events via messaging')
         ;
     }
