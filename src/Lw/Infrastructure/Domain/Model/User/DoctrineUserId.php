d266b46d0228a96eae7a19fa8e6f1dc5ffbb9a3e
Carlos Buenosvinos <carlos.buenosvinos@gmail.com>
2015-10-26 21:32:47 +0100

    Improving Entity/Identity (Doctrine 2.5 with CustomTypes - best option)

--- src/Lw/Infrastructure/Domain/Model/User/DoctrineUserId.php
+++ src/Lw/Infrastructure/Domain/Model/User/DoctrineUserId.php
@@ -2,12 +2,24 @@
 
 namespace Lw\Infrastructure\Domain\Model\User;
 
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\GuidType;
+use Lw\Domain\Model\User\UserId;
 
 class DoctrineUserId extends GuidType
 {
     public function getName()
     {
         return 'UserId';
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value->id();
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return new UserId($value);
     }
 }
--- src/Lw/Infrastructure/Domain/Model/Wish/DoctrineWishId.php
+++ src/Lw/Infrastructure/Domain/Model/Wish/DoctrineWishId.php
@@ -2,12 +2,24 @@
 
 namespace Lw\Infrastructure\Domain\Model\Wish;
 
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\GuidType;
+use Lw\Domain\Model\Wish\WishId;
 
 class DoctrineWishId extends GuidType
 {
     public function getName()
     {
         return 'WishId';
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value->id();
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return new WishId($value);
     }
 }
