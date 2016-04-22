7b6208fab1c2f0d75cf490cce7fad9a3214c714d
Carlos Buenosvinos <carlos.buenosvinos@gmail.com>
2016-04-22 22:04:51 +0200

    Fix #8

--- cli-config.php
+++ cli-config.php
@@ -1,8 +1,6 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 
-use Lw\Infrastructure\Persistence\Doctrine\EntityManagerFactory;
-
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
-    (new EntityManagerFactory())->build()
+    (new Lw\Infrastructure\Ui\Web\Silex\Application())->bootstrap()['em']
 );
